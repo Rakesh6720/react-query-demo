e92d933da4d9d4872c50d233d78e156d4954be2a
Rakesh Baruah <rbaruah@crisisprevention.com>
2024-01-15 12:02:10 -0600

    add query and mutate functions

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,7 +1,44 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { addTodo, fetchTodos } from "./api";
+import TodoCard from "./components/TodoCard";
+import { useState } from "react";
+
 export default function Demo() {
+  const [title, setTitle] = useState("");
+  const [search, setSearch] = useState("");
+  const queryClient = useQueryClient();
+  const {data: todos, isLoading} = useQuery({
+    queryFn: () => fetchTodos(search),
+    queryKey: ["todos", {search}]
+  });
+
+  const {mutateAsync: addTodoMutation} = useMutation({
+    mutationFn: addTodo,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["todos"]);
+    }
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div>
-        <b>React Query Demo</b>
+      <div>
+        <input type="text" onChange={(e) => setTitle(e.target.value)} value={title}/>
+        <button onClick={async () => {
+          try {
+            await addTodoMutation({title});
+            setTitle("");
+          } catch (e) {
+            console.error(e);
+          }
+        }}>Add Todo</button>
+      </div>
+      {todos?.map((todo) => {
+        return <TodoCard key={todo.id} todo={todo}/>
+      })}
     </div>
   )
 }
